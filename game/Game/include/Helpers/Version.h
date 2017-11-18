ccf42f9af1701ce1bfa2e63ef669c7b9c4a1f29d
n2omatt <n2omatt@amazingcow.com>
2017-11-18 15:56:35 -0200

    - Add the Version file.

---
+++ game/Game/include/Helpers/Version.h
@@ -0,0 +1,32 @@
+//~---------------------------------------------------------------------------//
+//                     _______  _______  _______  _     _                     //
+//                    |   _   ||       ||       || | _ | |                    //
+//                    |  |_|  ||       ||   _   || || || |                    //
+//                    |       ||       ||  | |  ||       |                    //
+//                    |       ||      _||  |_|  ||       |                    //
+//                    |   _   ||     |_ |       ||   _   |                    //
+//                    |__| |__||_______||_______||__| |__|                    //
+//                             www.amazingcow.com                             //
+//  File      : Version.h                                                     //
+//  Project   : Cosmic Intruders                                              //
+//  Date      : Nov 18, 2017                                                  //
+//  License   : GPLv3                                                         //
+//  Author    : n2omatt <n2omatt@amazingcow.com>                              //
+//  Copyright : AmazingCow - 2017                                             //
+//                                                                            //
+//  Description :                                                             //
+//                                                                            //
+//---------------------------------------------------------------------------~//
+
+#pragma once
+
+//------------------------------------------------------------------------------
+// The game version.
+#define COSMIC_INTRUDERS_VERSION_MAJOR    "1"
+#define COSMIC_INTRUDERS_VERSION_MINOR    "0"
+#define COSMIC_INTRUDERS_VERSION_REVISION "1"
+
+#define COSMIC_INTRUDERS_VERSION           \
+        COSMIC_INTRUDERS_VERSION_MAJOR "." \
+        COSMIC_INTRUDERS_VERSION_MINOR "." \
+        COSMIC_INTRUDERS_VERSION_REVISION
