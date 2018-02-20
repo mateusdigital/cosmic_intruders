f65404cb92f7de08e4ce3062e31123d695fd8bce
n2omatt <n2omatt@amazingcow.com>
2018-02-19 23:12:41 -0300

    - Update the update_version script to use the Amazing Cow's tool.

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -26,82 +26,16 @@
 ##---------------------------------------------------------------------------~##
 
 ##----------------------------------------------------------------------------##
-## CONFIG                                                                     ##
+## Imports                                                                    ##
 ##----------------------------------------------------------------------------##
-PROJECT_NAME="COSMIC_INTRUDERS"
-HEADER_FILE="game/Game/include/Helpers/Version.h" ## Relative to project root.
-
+source /usr/local/src/acow_shellscript_utils.sh
 
 ##----------------------------------------------------------------------------##
-## Constants                                                                  ##
+## Script                                                                     ##
 ##----------------------------------------------------------------------------##
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
+cd $(get_script_dir);
 
-
-##----------------------------------------------------------------------------##
-## Vars                                                                       ##
-##----------------------------------------------------------------------------##
-MAJOR=$(echo $@ | cut -d. -f1);
-MINOR=$(echo $@ | cut -d. -f2);
-REVISION=$(echo $@ | cut -d. -f3);
-
-
-##----------------------------------------------------------------------------##
-## Sanity                                                                     ##
-##----------------------------------------------------------------------------##
-## Thanks to Charles Duffy in SO:
-##    http://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
-re='^[0-9]+$' #Check if is number.
-
-if ! [[ $MAJOR =~ $re ]] ; then
-   echo "MAJOR Not a number" >&2;
-   exit 1
-fi
-
-if ! [[ $MINOR =~ $re ]] ; then
-   echo "MINOR Not a number" >&2;
-   exit 1
-fi
-
-if ! [[ $REVISION =~ $re ]] ; then
-   echo "REVISION Not a number" >&2;
-   exit 1
-fi
-
-
-##----------------------------------------------------------------------------##
-## Update HEADER FILE                                                         ##
-##----------------------------------------------------------------------------##
-##----------------------------------------------------------------------------##
-## Make sure that we're on project root.
-cd "$SCRIPT_DIR/..";
-
-##----------------------------------------------------------------------------##
-## Make the update.
-MAJOR_STR="#define ${PROJECT_NAME}_VERSION_MAJOR    ";
-MINOR_STR="#define ${PROJECT_NAME}_VERSION_MINOR    ";
-REVISION_STR="#define ${PROJECT_NAME}_VERSION_REVISION ";
-
-cat $HEADER_FILE \
-    | sed -e s/"$MAJOR_STR\".*\""/"$MAJOR_STR\"$MAJOR\""/g          \
-    | sed -e s/"$MINOR_STR\".*\""/"$MINOR_STR\"$MINOR\""/g          \
-    | sed -e s/"$REVISION_STR\".*\""/"$REVISION_STR\"$REVISION\""/g \
-    > $HEADER_FILE.new;
-
-
-##----------------------------------------------------------------------------##
-## CHECKING                                                                   ##
-##----------------------------------------------------------------------------##
-## CHECK IF OPERATION WAS OK ##
-cat $HEADER_FILE.new;
-
-echo "Is this correct?[y/n]";
-read CORRECT;
-
-if [ "$CORRECT" = "y" ]; then
-    echo "Updating the files..."
-    mv $HEADER_FILE.new  $HEADER_FILE;
-else
-  rm $HEADER_FILE.new
-fi;
-
+update_version                             \
+    "COSMIC_INTRUDERS"                     \
+    "$1"                                   \
+    ../game/Game/include/Helpers/Version.h
