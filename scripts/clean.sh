84026b58d6e0a403adfbc746d61c3bd229cabcfa
n2omatt <n2omatt@amazingcow.com>
2018-02-19 23:11:42 -0300

    - Add a simple script to let the project's root directory clean.

---
+++ scripts/clean.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+##~---------------------------------------------------------------------------##
+##                     _______  _______  _______  _     _                     ##
+##                    |   _   ||       ||       || | _ | |                    ##
+##                    |  |_|  ||       ||   _   || || || |                    ##
+##                    |       ||       ||  | |  ||       |                    ##
+##                    |       ||      _||  |_|  ||       |                    ##
+##                    |   _   ||     |_ |       ||   _   |                    ##
+##                    |__| |__||_______||_______||__| |__|                    ##
+##                             www.amazingcow.com                             ##
+##  File      : clean.sh                                                      ##
+##  Project   : CosmicIntruders                                               ##
+##  Date      : Feb 19, 2018                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : n2omatt <n2omatt@amazingcow.com>                              ##
+##  Copyright : AmazingCow - 2018                                             ##
+##                                                                            ##
+##  Description :                                                             ##
+##                                                                            ##
+##---------------------------------------------------------------------------~##
+
+##----------------------------------------------------------------------------##
+## Imports                                                                    ##
+##----------------------------------------------------------------------------##
+source /usr/local/src/acow_shellscript_utils.sh
+cd $(get_script_dir);
+
+echo "Cleanning everything...";
+rm -vrf ../build;
+
+rm -vrf ../web_release_*;
+rm -vrf ../gnu_linux_release_*;
+
+rm -vrf ../web_debug_*;
+rm -vrf ../gnu_linux_debug_*;
