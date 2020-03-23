557f317ab95a3b503703232185dcc3694ccb584f
stdmatt <stdmatt@pixelwizards.io>
2020-03-23 17:17:22 +0300

    Start to fix the build script for web

--- scripts/platforms/build_desktop.sh
+++ scripts/platforms/build_desktop.sh
@@ -25,30 +25,21 @@ source /usr/local/src/stdmatt/shellscript_utils/main.sh
 ##----------------------------------------------------------------------------##
 ## Vars                                                                       ##
 ##----------------------------------------------------------------------------##
-# OS_NAME=$(pw_os_get_simple_name);
-
-# MODE="$1";
-# CMAKE_ROOT="$(pw_realpath $2)";
-# BUILD_DIR="$(pw_realpath $3/${OS_NAME}_${MODE})";
-# RELEASE_DIR="$(pw_realpath $4/${OS_NAME}_${MODE})";
-# VERSION="$6";
 SCRIPT_DIR="$(pw_get_script_dir)";
 PLATFORM_NAME="$(pw_os_get_simple_name)";
 BUILD_DIR="${PROJECT_BUILD_DIR}/${PROJECT_NAME}_${PLATFORM_NAME}_${PROJECT_VERSION}";
+
 
 ##----------------------------------------------------------------------------##
 ## Script                                                                     ##
 ##----------------------------------------------------------------------------##
 echo "Desktop Build";
 echo "Building at: ${BUILD_DIR}";
-
-##
-## Go to the build directory, this eases the cmake operations...
 mkdir -p "$BUILD_DIR";
 
-cmake "${SCRIPT_DIR}/CMakelists.txt"           \
+cmake "${SCRIPT_DIR}/CMakelists.txt"            \
     -B "${BUILD_DIR}"                           \
-    -D -DCMAKE_BUILD_TYPE=$"PROJECT_BUILD_MODE";
+    -D -DCMAKE_BUILD_TYPE="$PROJECT_BUILD_MODE";
 
 cd "${BUILD_DIR}";
     cmake --build .
