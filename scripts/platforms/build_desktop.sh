a071810bab82fcb496da0d21507c4acff61c9ecd
stdmatt <stdmatt@pixelwizards.io>
2020-06-16 12:52:54 +0300

    Make the project builds with the GNU/Linux name under WSL.

--- scripts/platforms/build_desktop.sh
+++ scripts/platforms/build_desktop.sh
@@ -10,7 +10,7 @@
 ##  Date      : Nov 17, 2017                                                  ##
 ##  License   : GPLv3                                                         ##
 ##  Author    : stdmatt <stdmatt@pixelwizards.io>                             ##
-##  Copyright : stdmatt - 2017 - 2019                                         ##
+##  Copyright : stdmatt - 2017 - 2020                                         ##
 ##                                                                            ##
 ##  Description :                                                             ##
 ##                                                                            ##
@@ -27,6 +27,10 @@ source /usr/local/src/stdmatt/shellscript_utils/main.sh
 ##----------------------------------------------------------------------------##
 SCRIPT_DIR="$(pw_get_script_dir)";
 PLATFORM_NAME="$(pw_os_get_simple_name)";
+if [ "$PLATFORM_NAME" == "$(PW_OS_WSL)" ]; then
+    PLATFORM_NAME="$(PW_OS_GNU_LINUX)";
+fi;
+
 BUILD_DIR="${PROJECT_BUILD_DIR}/${PROJECT_NAME}_${PLATFORM_NAME}_${PROJECT_VERSION}";
 
 
