8b3087a48303efe0d3f173131d8eb1d8ccf723bf
mateus-earth <matt@cosmicpig.digital>
2022-09-22 15:10:52 -0300

    Trying to make build for web

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,103 +11,66 @@ LIBS_ROOT_DIR="${ROOT_DIR}/lib/Cooper";
 GAME_ROOT_DIR="${ROOT_DIR}/game";
 ASSETS_DIR="${ROOT_DIR}/assets";
 
-BUILD_TYPE="Release";
-BUILD_PC="";
-BUILD_WEB="";
 PACKAGE_BUILD="";
 
-GAME_VERSION=$(cat "${ROOT_DIR}/game/version" | tr -d "\"");
-PLATFORM_NAME=$(uname)
+GAME_NAME="cosmic-intruders";
+GAME_VERSION=$(cat "${ROOT_DIR}/game/version" | tr -d "\""); ## without any quotes...
+GAME_BUILD_TYPE="Release";
+GAME_BUILD_TARGET="pc";
+GAME_EMSCRIPTEN_TOOLCHAIN="${HOME}/.emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake";
 
+PLATFORM_NAME=$(uname)
 
 while true; do
     case "$1" in
-        "--release" ) BUILD_TYPE="Release"; ;;
-        "--debug"   ) BUILD_TYPE="Debug";   ;;
-        "--pc"      ) BUILD_PC="true";      ;;
-        "--web"     ) BUILD_WEB="true";     ;;
-        "--package" ) PACKAGE_BUILD="true"; ;;
-        *) show_help;                       ;;
+        "--release" ) GAME_BUILD_TYPE="Release"; ;;
+        "--debug"   ) GAME_BUILD_TYPE="Debug";   ;;
+        "--pc"      ) GAME_BUILD_TARGET="pc";    ;;
+        "--web"     ) GAME_BUILD_TARGET="web";   ;;
+        "--package" ) PACKAGE_BUILD="true";      ;;
+        *) show_help;                            ;;
     esac;
     shift;
-    if [ $# -eq 0 ]; then 
+    if [ $# -eq 0 ]; then
         break;
     fi;
 done;
 
-## 
-## Run Info
-##
-
-echo "SCRIPT_DIR    : $SCRIPT_DIR";
-echo "ROOT_DIR      : $ROOT_DIR";
-echo "BUILD_DIR     : $BUILD_DIR";
-echo "DIST_DIR      : $DIST_DIR";
-echo "BUILD_TYPE    : $BUILD_TYPE";
-echo "PACKAGE_BUILD : $PACKAGE_BUILD";
 
 ##
 ## Build the game.
 ##
 
-if [ -n "${BUILD_PC}" ]; then 
-    cd "${ROOT_DIR}" || exit; ## Make things relatie to root is easier...
-    mkdir -p "${BUILD_DIR}"                                  \
-        && cd "${BUILD_DIR}"                                 \
-        && cmake                                             \
-            -DCMAKE_BUILD_TYPE="${BUILD_TYPE}"               \
-            -DGAME_VERSION="\"${GAME_VERSION}\""             \
-            ..                                               \
-        && cmake                                             \
-            --build .                                        \
-            --config "${BUILD_TYPE}"                         \
+function build_game()
+{
+    local target_platform="$1";
+    echo "Building game for platform: ${target_platform}";
+
+    mkdir -p "${BUILD_DIR}"                                            \
+        && cd "${BUILD_DIR}"                                           \
+        && cmake                                                       \
+            -DGAME_NAME="${GAME_NAME}"                                 \
+            -DGAME_VERSION="\"${GAME_VERSION}\""                       \
+            -DGAME_TARGET_PLATFORM="${target_platform}"                \
+            -DCMAKE_BUILD_TYPE="${GAME_BUILD_TYPE}"                    \
+            -DGAME_EMSCRIPTEN_TOOLCHAIN="${GAME_EMSCRIPTEN_TOOLCHAIN}" \
+            ..                                                         \
+        && cmake                                                       \
+            --build .                                                  \
+            --config "${BUILD_TYPE}"                                   \
         ;
-fi;
+}
+export CMAKE_TOOLCHAIN_FILE="$HOME/.emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake";
 
+test "${GAME_BUILD_TARGET}" == "pc"   && build_game "pc";
+test "${GAME_BUILD_TARGET}" == "web"  && build_game "web";
 
-##
-## Build for web.
-##
-
-if [ -n "${BUILD_WEB}" ]; then 
-    ## Decide the flags that we gonna pass to em++
-    CXX_FLAGS="-O3 -DNDEBUG";
-    if [ "${BUILD_TYPE}" == "Debug" ]; then
-        CXX_FLAGS="-g -DCOOPER_DEBUG";
-    fi;
-
-    SOURCES="$(find "$LIBS_ROOT_DIR" -iname "*.cpp") ";
-    SOURCES+="$(find "$GAME_ROOT_DIR" -iname "*.cpp")"
-    SOURCES="$(echo "$SOURCES" | tr [:space:] " ")";
-
-    cd "${ROOT_DIR}"                                    \
-        && em++                                         \
-            ${CXX_FLAGS}                                \
-            -O3                                         \
-            -std=c++14                                  \
-            -I"${LIBS_ROOT_DIR}"                        \
-            -I"${LIBS_ROOT_DIR}/Cooper"                 \
-            -I"${GAME_ROOT_DIR}"                        \
-            -lidbfs.js                                  \
-            -DEMSCRIPTEN                                \
-            -o "${BUILD_DIR}/CosmicIntruders.html"      \
-            "${SOURCES}"                                \
-            --embed-file "./assets"                     \
-            -s WASM=0                                   \
-            -s USE_SDL=2                                \
-            -s USE_SDL_TTF=2                            \
-            -s USE_SDL_IMAGE=2                          \
-            -s SDL2_IMAGE_FORMATS='["png"]'             \
-            -s LEGACY_GL_EMULATION=0                    \
-            -s NO_EXIT_RUNTIME=1                        \
-        ;
-fi;
 
 ##
 ## Package if needed...
 ##
 
-if [ -n "$PACKAGE_BUILD" ]; then 
+if [ -n "$PACKAGE_BUILD" ]; then
     GAME_DIST_DIR="${DIST_DIR}/cosmic-intruders-${GAME_VERSION}-${PLATFORM_NAME}-${BUILD_TYPE}";
     GAME_ZIP_FILENAME="${DIST_DIR}/cosmic-intruders-${GAME_VERSION}-${PLATFORM_NAME}.zip";
 
