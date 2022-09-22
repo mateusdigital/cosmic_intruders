ef1ca58de01c8426eb1467ab68d3025f7214e652
mateus-earth <matt@cosmicpig.digital>
2022-09-22 12:17:49 -0300

    Start to build for web...

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,17 +7,33 @@ ROOT_DIR="$(dirname "$SCRIPT_DIR")";
 BUILD_DIR="${ROOT_DIR}/build";
 DIST_DIR="${ROOT_DIR}/dist";
 
+LIBS_ROOT_DIR="${ROOT_DIR}/lib/Cooper";
+GAME_ROOT_DIR="${ROOT_DIR}/game";
+ASSETS_DIR="${ROOT_DIR}/assets";
+
 BUILD_TYPE="Release";
+BUILD_PC="";
+BUILD_WEB="";
 PACKAGE_BUILD="";
 
 GAME_VERSION=$(cat "${ROOT_DIR}/game/version" | tr -d "\"");
 PLATFORM_NAME=$(uname)
 
-if [ "$1" == "--debug" ]; then 
-    BUILD_TYPE="Debug";
-elif [ "$1" == "--package" ]; then
-    PACKAGE_BUILD="true";
-fi;
+
+while true; do
+    case "$1" in
+        "--release" ) BUILD_TYPE="Release"; ;;
+        "--debug"   ) BUILD_TYPE="Debug";   ;;
+        "--pc"      ) BUILD_PC="true";      ;;
+        "--web"     ) BUILD_WEB="true";     ;;
+        "--package" ) PACKAGE_BUILD="true"; ;;
+        *) show_help;                       ;;
+    esac;
+    shift;
+    if [ $# -eq 0 ]; then 
+        break;
+    fi;
+done;
 
 ## 
 ## Run Info
@@ -34,24 +50,64 @@ echo "PACKAGE_BUILD : $PACKAGE_BUILD";
 ## Build the game.
 ##
 
-## cd "${ROOT_DIR}" || exit; ## Make things relatie to root is easier...
-## mkdir -p "${BUILD_DIR}"                                  \
-##     && cd "${BUILD_DIR}"                                 \
-##     && cmake                                             \
-##         -DCMAKE_BUILD_TYPE="${BUILD_TYPE}"               \
-##         -DGAME_VERSION="\"${GAME_VERSION}\""             \
-##         ..                                               \
-##     && cmake                                             \
-##         --build .                                        \
-##         --config "${BUILD_TYPE}"                         \
-##     ;
+if [ -n "${BUILD_PC}" ]; then 
+    cd "${ROOT_DIR}" || exit; ## Make things relatie to root is easier...
+    mkdir -p "${BUILD_DIR}"                                  \
+        && cd "${BUILD_DIR}"                                 \
+        && cmake                                             \
+            -DCMAKE_BUILD_TYPE="${BUILD_TYPE}"               \
+            -DGAME_VERSION="\"${GAME_VERSION}\""             \
+            ..                                               \
+        && cmake                                             \
+            --build .                                        \
+            --config "${BUILD_TYPE}"                         \
+        ;
+fi;
 
+
+##
+## Build for web.
+##
+
+if [ -n "${BUILD_WEB}" ]; then 
+    ## Decide the flags that we gonna pass to em++
+    CXX_FLAGS="-O3 -DNDEBUG";
+    if [ "${BUILD_TYPE}" == "Debug" ]; then
+        CXX_FLAGS="-g -DCOOPER_DEBUG";
+    fi;
+
+    SOURCES="$(find "$LIBS_ROOT_DIR" -iname "*.cpp") ";
+    SOURCES+="$(find "$GAME_ROOT_DIR" -iname "*.cpp")"
+    SOURCES="$(echo "$SOURCES" | tr [:space:] " ")";
+
+    cd "${ROOT_DIR}"                                    \
+        && em++                                         \
+            ${CXX_FLAGS}                                \
+            -O3                                         \
+            -std=c++14                                  \
+            -I"${LIBS_ROOT_DIR}"                        \
+            -I"${LIBS_ROOT_DIR}/Cooper"                 \
+            -I"${GAME_ROOT_DIR}"                        \
+            -lidbfs.js                                  \
+            -DEMSCRIPTEN                                \
+            -o "${BUILD_DIR}/CosmicIntruders.html"      \
+            "${SOURCES}"                                \
+            --embed-file "./assets"                     \
+            -s WASM=0                                   \
+            -s USE_SDL=2                                \
+            -s USE_SDL_TTF=2                            \
+            -s USE_SDL_IMAGE=2                          \
+            -s SDL2_IMAGE_FORMATS='["png"]'             \
+            -s LEGACY_GL_EMULATION=0                    \
+            -s NO_EXIT_RUNTIME=1                        \
+        ;
+fi;
 
 ##
 ## Package if needed...
 ##
 
-if [ "$PACKAGE_BUILD" == "true" ]; then 
+if [ -n "$PACKAGE_BUILD" ]; then 
     GAME_DIST_DIR="${DIST_DIR}/cosmic-intruders-${GAME_VERSION}-${PLATFORM_NAME}-${BUILD_TYPE}";
     GAME_ZIP_FILENAME="${DIST_DIR}/cosmic-intruders-${GAME_VERSION}-${PLATFORM_NAME}.zip";
 
