58d2db61f5a29d5246b4ad622df60b8b9f3a56a5
mateusdigital <mateus@cosmicpig.digital>
2023-06-06 18:13:18 -0300

    Add the possibility of package the game for windows

--- scripts/generate-release-zip.sh
+++ scripts/generate-release-zip.sh
@@ -24,29 +24,37 @@
 set -e; ## break on errors.
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
-readonly ROOT_DIR="$(dirname "$SCRIPT_DIR")";
+readonly SCRIPT_ROOT_DIR="$(dirname "$SCRIPT_DIR")";
 
-platform_name="gnu_linux"; ## @todo: deal with windows later...
+if [ ! -z "$WINDOWS_REPO_ROOT" ]; then
+    echo "Windows hack build...";
+    readonly ROOT_DIR="$WINDOWS_REPO_ROOT";
+    readonly PLATFORM_NAME="windows";
+else
+    readonly ROOT_DIR="$(dirname "$SCRIPT_DIR")";
+    readonly PLATFORM_NAME="gnu_linux";
+fi;
+
 if [ -n "$(uname -a | grep -i "Darwin")" ]; then
-    platform_name="macOS";
+    PLATFORM_NAME="macOS";
 fi;
 
 readonly PROJECT_NAME="cosmic-intruders";
 readonly PROJECT_VERSION="$(git describe --abbrev=0 --tags)";
 readonly FULL_PROJECT_NAME="${PROJECT_NAME}_${PROJECT_VERSION}";
 
-readonly OUTPUT_DIR="${ROOT_DIR}/dist/${FULL_PROJECT_NAME}";
-readonly ZIP_FULL_PATH="${ROOT_DIR}/dist/${FULL_PROJECT_NAME}_${platform_name}.zip";
+readonly OUTPUT_DIR="${SCRIPT_ROOT_DIR}/dist/${FULL_PROJECT_NAME}";
+readonly ZIP_FULL_PATH="${SCRIPT_ROOT_DIR}/dist/${FULL_PROJECT_NAME}_${PLATFORM_NAME}.zip";
 
 
-echo "$0 ==> Generating release zip ($platform_name)...";
+echo "$0 ==> Generating release zip ($PLATFORM_NAME)...";
 
 ## Create the directory.
 rm    -rf   "${OUTPUT_DIR}";
 mkdir -p "${OUTPUT_DIR}";
 
 ## Copy resources.
-cp -v    "${ROOT_DIR}/build-pc-Release/${PROJECT_NAME}" "${OUTPUT_DIR}";
+cp -v    "${ROOT_DIR}/build-pc-Release/"*               "${OUTPUT_DIR}";
 cp -v -R "${ROOT_DIR}/assets/"                          "${OUTPUT_DIR}/assets";
 cp -v    "${ROOT_DIR}/resources/readme-release.txt"     "${OUTPUT_DIR}";
 
